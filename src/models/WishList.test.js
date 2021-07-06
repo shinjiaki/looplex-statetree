d74a032f0ebd3f129f566f447495e3405be6d694
shinjiaki <julio_shinji@hotmail.com>
2021-07-06 20:44:12 -0300

    class 4

--- src/models/WishList.test.js
+++ src/models/WishList.test.js
@@ -1,5 +1,6 @@
 import { getSnapshot, onSnapshot, onPatch } from "mobx-state-tree"
-import { WishListItem, WishList } from "./WishList"
+import { reaction } from "mobx"
+import { WishList, WishListItem } from "./WishList"
 
 it("can create a instance of a model", () => {
   const item = WishListItem.create({
@@ -65,4 +66,33 @@ it("can add new items", () => {
   expect(getSnapshot(list)).toMatchSnapshot()
 
   expect(states).toMatchSnapshot()
+})
+
+it("can calculate the total price of a wishlist", () => {
+  const list = WishList.create({
+    items: [
+      {
+        name: "Machine Gun Preacher",
+        price: 7.35,
+        image: "https://images-na.ssl-images-amazon.com/images/S/pv-target-images/f4086baf7ba1ee2117a70d40b6debb4f5f5cc65ab7c5e84323855b62b43bb4ed._RI_V_TTW_.jpg"
+      },
+      {
+        name: "LEGO Mindstorm EV3",
+        price: 349.95,
+        image: "https://www.lego.com/cdn/cs/set/assets/blt7fc03b671602b2fa/31313_alt1.jpg?fit=bounds&format=jpg&quality=80&width=320&height=320&dpr=1"
+      }
+    ]
+  })
+
+  expect(list.totalPrice).toBe(357.3)
+
+  let changed = 0
+  reaction(() => list.totalPrice, () => changed++)
+
+  expect(changed).toBe(0)
+  console.log(list.totalPrice)
+  list.items[0].changeName("Test")
+  expect(changed).toBe(0)
+  list.items[0].changePrice(10)
+  expect(changed).toBe(1)
 })
