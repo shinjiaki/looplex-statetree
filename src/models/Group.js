5e7e4435103e2643184cc0648baf0a8c1fd70134
shinjiaki <julio_shinji@hotmail.com>
2021-07-07 13:44:04 -0300

    class 14

--- src/models/Group.js
+++ src/models/Group.js
@@ -1,4 +1,4 @@
-import { types, flow } from "mobx-state-tree"
+import { types, flow, applySnapshot } from "mobx-state-tree"
 
 import { WishList } from "./WishList"
 
@@ -16,10 +16,18 @@ const User = types.model({
   })
 }))
 
-export const Group = types.model({
+export const Group = types
+.model({
   users: types.map(User)
 })
 .actions(self => ({
+  afterCreate() {
+    self.load()
+  },
+  load: flow(function* load() {
+    const response = yield window.fetch(`http://localhost:3001/users`)
+    applySnapshot(self.users, yield response.json())
+  }),
   drawLots() {
     const allUsers = Array.from(self.users.values())
 
